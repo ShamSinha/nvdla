b49866479ece8d6c40ba089a2f3b6a4370025cde
nvdlagroup <nvdlagroup@gmail.com>
2019-07-06 15:31:40 +0900

    add matrix_concat

--- datacube.py
+++ datacube.py
@@ -9,27 +9,42 @@
 import pprint
 import random
 class Datacube():
+	
 	atomic_5d = [[[[[]]]]]  # empty atomic cube data structure unique to every data cube 
-	def __init__(self, width, height, channels, n_cubes=1):
+	
+
+	def __init__(self, width, height, channels, pres, pad=[0,0,0,0], n_cubes=1 ):
 		self.width = width
 		self.height = height
 		self.channels = channels
 		self.n_cubes = n_cubes
-
+		self.pres = pres
+		self.pad = pad
+	
 	def define_precision(self):
-		pass
-
+		if self.pres == 'int8':
+			pass
+		elif self.pres == 'int16':
+			pass
+		elif self.pres == 'fp16':
+			pass 
+		elif self.pres == 'fp32':
+			pass
+		else:
+			print("Invalid choice. Not supported!!")
+	
 	def dimensions(self):
 		'''define the dimensions of atomic cubes across all cubes '''
 
 		channel_size_atomic = 64 
-		blocks = self.channels/channel_size_atomic
+		blocks = self.channels/channel_size_atomic  # FIX here, n_atomic_* go to 0 when channel size < 64
 		n_atomic_per_cubes = int(self.width*self.height*blocks)
 		n_atomic_all_cubes = int(n_atomic_per_cubes*self.n_cubes)
 		
 		return n_atomic_per_cubes, n_atomic_all_cubes
 
-	def zero_concat(self):
+	
+	def zero_concat_channels(self):  
 		''' If channel size is not a multiple of 64 we append zeros 
 			this function will generate the appropriate number of zero channels to be added
 		'''
@@ -39,8 +54,40 @@ class Datacube():
 				return zero_concat_channels
 			else:
 				continue
-			return self.channels
+			return self.channels  
 	
+	
+	def zero_concat_cube(self):
+		'''return zero data cube '''
+		atomic_width = 1
+		atomic_height = 1
+		channel_size_atomic = 64 
+		zero_concat_channels = self.zero_concat_channels()  # number of channels with zero values
+		print(zero_concat_channels)
+		zero_cube = [[[[[0 for i in range(atomic_width)] for j in range(atomic_height)] for k in range(channel_size_atomic)] for l in range(zero_concat_channels)] for m in range(self.n_cubes)]
+
+		return zero_cube
+
+	def sum_cubes(self, zero, atomic):  # Bug in function 
+		''' concatenate two atomic data cubes when the channel dimension is not a multiple of 
+			64 
+			input:
+				original atomic cube = atomic
+				zero data atomic cube = zero
+			output:
+				zero concatenated atomic cube = atomic 
+		'''
+		# Addition will take place in the channel dimension all things being the same
+		length_atomic = self.channels 
+		length_zero = len(zero[0][0][:][0][0])
+		total_length = length_zero + length_atomic # total channel length after zero addition 
+		for cube_no in range(self.n_cubes):
+			for atomic_no_ in range(self.height*self.width):
+				#for channel_no in range(length_atomic,total_length):
+				atomic[0][0][:][atomic_no_][cube_no].append(zero[0][0][:][atomic_no_][cube_no])
+
+		return atomic 
+
 	def initialize_atomic_cubes(self):
 		'''initialize the atomic data cubes given the data cube dimensions
 			input :
@@ -59,39 +106,57 @@ class Datacube():
 		
 		if blocks.is_integer():
 			self.atomic_5d = [[[[[random.random() for i in range(atomic_width)] for j in range(atomic_height)] for k in range(channel_size_atomic)] for l in range(n_atomic_per_cubes)] for m in range(self.n_cubes)]
+			
 		else:
-			zero_concat_channels = self.zero_concat()
-			print(zero_concat_channels)
+			zero_cube = self.zero_concat_cube()
+			self.atomic_5d = [[[[[random.random() for i in range(atomic_width)] for j in range(atomic_height)] for k in range(channel_size_atomic)] for l in range(n_atomic_per_cubes)] for m in range(self.n_cubes)]
+			self.atomic_5d = self.sum_cubes(zero_cube, self.atomic_5d)
 
-			self.atomic_5d = [[[[[random.random() for i in range(atomic_width)] for j in range(atomic_height)] for k in range(channel_size_atomic)] for l in range(zero_concat_channels)] for m in range(self.n_cubes)]
-
+			# zero_concat_channels = self.zero_concat_channels() 
+			# self.atomic_5d = [[[[[random.random() for i in range(atomic_width)] for j in range(atomic_height)] for k in range(channel_size_atomic)] for l in range(zero_concat_channels)] for m in range(self.n_cubes)]
 		return self.atomic_5d
 
+	
 	def print_values(self):
 		'''print the dimensions of the data cube '''
 
 		print(self.width, " ", self.height, " ", self.channels, " ", self.n_cubes)
-
+		print(self.pad[0], " ", self.pad[1], " ", self.pad[2], " ", self.pad[3])
 
 def ask_user():
+	''' test code for above'''
 
-	w = int(input("Enter width of data cube"))
-	h = int(input("Enter height of data cube"))
-	c = int(input("Enter channel size of data cube"))
-	choice = input("Enter choice of data cube (only for kernel press 'k')")
-	if (choice == 'k'):
-		n = int(input("Enter number of cubes (relevant for kernel only, defaults to 1)"))
-	else:
-		n =1
+	try:
+		w = int(input("Enter width of data cube"))
+		h = int(input("Enter height of data cube"))
+		c = int(input("Enter channel size of data cube"))
+		choice = input("Enter choice of data cube (only for kernel press 'k')")
+		if (choice == 'k'):
+			n = int(input("Enter number of cubes (relevant for kernel only, defaults to 1)"))
+		else:
+			n =1
+		pres = input("Enter the precision")
+		choice = input("Padding?? (y/n)")
+		if(choice == 'y'):
+			left = int(input("Enter left padding "))
+			right = int(input("Enter right padding"))
+			top  = int(input("Enter top padding"))
+			bottom = int(input("Enter bottom padding"))
+		else:
+			[left,right,top,bottom] = [0,0,0,0]
+			pad = [left, right, top , bottom]
+		input_cube = Datacube(w,h,c,pres,pad,n)
+		input_cube.print_values()
+		atmoics = input_cube.initialize_atomic_cubes()
+		pprint.pprint(atmoics[:][:][:][0][0])
+		per , total = input_cube.dimensions()
+		print("Number of atomic cells per cube: {}".format(per))
+		print("Number of atomic cells in total: {}".format(total))
+		print('channel dimension length of atomic cubes for this input cube : {}'.format(len(input_cube.atomic_5d[:][:][:][0][0])))
 
-	input_cube = Datacube(w,h,c,n)
-	input_cube.print_values()
-	atmoics = input_cube.initialize_atomic_cubes()
-	pprint.pprint(atmoics[:][:][:][0][0])
-	per , total = input_cube.dimensions()
-	print("Number of atomic cells per cube: {}".format(per))
-	print("Number of atomic cells in total: {}".format(total))
-	print('channel dimension length of atomic cubes for this input cube : {}'.format(len(input_cube.atomic_5d[:][:][:][0][0])))
+		
+	except Exception as e:
+		print (e.message , e.args)
 
 
 if __name__ == '__main__':
---
+++ matrix_concat.py
@@ -0,0 +1,110 @@
+#!/usr/local/bin/python3
+''' matrix concatenation along different dimensions '''
+import random
+import sys
+def create_matrix():
+	''' define 5d matrices   '''
+	mat_1 = [[[[[round(random.random(),3) for i in range(1)] for j in range(1)] for k in range(64)] for l in range(18)] for m in range(2)]
+	mat_2 = [[[[[0 for i in range(1)] for j in range(1)] for k in range(64)] for l in range(18)] for m in range(2)]
+	mat_3 = [[[[[0 for i in range(1)] for j in range(1)] for k in range(64)] for l in range(1)] for m in range(2)]
+	check_type(mat_1)
+	check_dims(mat_1, mat_2)
+	check_dims(mat_1, mat_3)
+
+	return mat_1, mat_2, mat_3
+
+i = 1
+def check_dims(m_1, m_2):
+	''' check for right dimension size '''
+	global i
+	dims_1 = dimensions(m_1)
+	dims_2 = dimensions(m_2)
+	try:
+		d_1 = len(dims_1)
+		d_2 = len(dims_2)
+		if d_1 == d_2:
+			#concatenation is possible
+			print("Concatenation " + str(i) + " can proceed")
+			i += 1
+		else:
+			raise Exception("The dimensions dont match can't concatenate..")
+	except Exception as e:
+		print(e.message, e.args)
+ 
+
+def matrix_concat_selection():
+	''' choose which side to concatenate along '''
+	choice_ques = ''' Which side to concatenate along....
+	1) Press c for channel direction
+	2) Press l for left side direction
+	3) Press r for right side direction
+	4) Press t for top side direction 
+	5) Press b for bottom side direction 
+	6) Press e to exit this exercise 
+	Enter choice = '''
+	choice = input(choice_ques)
+	return choice
+
+def matrix_concat(c,m_1,m_2,m_3):
+	''' perform concatenation based on choice '''
+	if (c == 'c'):
+		print("Concatenating along channel direction")
+		concat_mat = channel(m_1,m_2)
+
+	elif(c == 'l'):
+		print("Concatenate along left direction")
+		concat_mat = left(m_1, m_3)
+	elif(c == 'r'):
+		print("Concatenate along right direction")
+		concat_mat = right(m_1, m_3)
+	elif(c == 't'):
+		print("Concatenate along top direction")
+		concat_mat = top(m_1, m_3)
+	elif(c == 'b'):
+		print("Concatenate along bottom direction")
+		concat_mat = bottom(m_1, m_3)
+	
+	return concat_mat
+
+def channel(m,n):
+	d_m = dimensions(m)
+	d_n = dimensions(n)
+	zero_concat = [[ [] for i in range(d_m[3])] for j in range(d_m[4])]
+	print(zero_concat)
+	for c_n in range(d_m[4]):
+		for a_n in range(d_m[3]):	
+			li = m[c_n][a_n][:][:][:]
+			ap = n[c_n][a_n][:][:][:]
+			li.extend(ap)
+			zero_concat[c_n][a_n].append(li)
+
+	return zero_concat
+
+def dimensions(mat):
+	'''find the dimension lengths '''
+	zero_th =len(mat[:][0][0][0][0])  # width axis
+	first =  len(mat[:][:][0][0][0])  # height axis
+	second = len(mat[:][:][:][0][0])  # channel axis
+	third =  len(mat[:][:][:][:][0])  # atom cube number per cube
+	fourth = len(mat[:][:][:][:][:])  # cube number
+	dims = [zero_th,first,second,third,fourth]
+	return dims
+
+def check_type(mat):
+	#print(type(mat[:][:][:][0][0]))
+   	#print(len(mat[:][:][:][0][:]))
+	#print(mat[0][0][:][:][:])
+	#print(mat[0][0][0][:][:])
+	pass
+
+def main():
+	m_1, m_2, m_3 = create_matrix()
+	choice = matrix_concat_selection()
+	if choice == 'e':
+		sys.exit(0)
+
+	m_out = matrix_concat(choice,m_1,m_2,m_3)
+	dims_out = dimensions(m_out)
+	print(dims_out)
+if __name__ == '__main__':
+	main()
